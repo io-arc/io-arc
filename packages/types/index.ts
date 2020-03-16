4190209c2b2af047319e017730b9d6de20cfd4cb
Nobuyuki Kondo <kondo@arc-one.jp>
2020-03-17 07:59:55 +0900

    feat: global TypeScript types

---
+++ packages/types/index.ts
@@ -0,0 +1,20 @@
+/** Directory name with slash for last */
+export type TDirName = string
+
+/** Directory name with no slash for last */
+export type TDirNameKey = string
+
+/** Filename with extension */
+export type TFileName = string
+
+/** Filename with no extension */
+export type TFileNameKey = string
+
+/** Full filepath with extension */
+export type TFilePath = string
+
+/** Task name */
+export type TTaskName = string
+
+/** Glob pattern */
+export type TGlobPattern = string
