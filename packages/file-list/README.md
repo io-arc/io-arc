7f58b73332e116b4a85e835c7da57e8ee24cd4fb
Nobuyuki Kondo <kondo@arc-one.jp>
2020-03-21 17:00:47 +0900

    feat: Adding file-list logic

---
+++ packages/file-list/README.md
@@ -0,0 +1,11 @@
+# `@io-arc/file-list`
+
+Get file list
+
+## Usage
+
+```
+import { FileList } from '@io-arc/file-list'
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/file-list/src/index.ts
@@ -0,0 +1,28 @@
+import glob from 'glob'
+import { TDirNameKey, TFilePath } from '@io-arc/types'
+
+/**
+ * Get file list for directory
+ * @param dir - directory
+ * @param ext - file extension
+ * @param rootOnly - one level only
+ * @constructor
+ */
+export const FileList = (
+  dir: TDirNameKey,
+  ext: string,
+  rootOnly = false
+): { [p: string]: TFilePath } => {
+  const target = rootOnly ? `${dir}/[!_]*.${ext}` : `${dir}/**/[!_]*.${ext}`
+  const files = glob.sync(target)
+  const reg1 = new RegExp(`${dir}/`)
+  const reg2 = new RegExp(`.${ext}$`)
+  const entries: { [p: string]: TFilePath } = {}
+
+  files.forEach((file: TFilePath) => {
+    const key = file.replace(reg1, '').replace(reg2, '')
+    entries[key] = file
+  })
+
+  return entries
+}
