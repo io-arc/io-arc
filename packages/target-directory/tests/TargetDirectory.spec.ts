c46b0fbd232bd87994e4938427ca6d7726290296
Nobuyuki Kondo <kondo@arc-one.jp>
2020-03-27 14:11:02 +0900

    docs: Adding document

--- packages/target-directory/tests/TargetDirectory.spec.ts
+++ packages/target-directory/tests/TargetDirectory.spec.ts
@@ -1,26 +1,26 @@
 import TargetDirectory from '../src'
 
-test('Avail working directory array', () => {
+test('Working directory array for key exists', () => {
   const res = TargetDirectory.wsArray('wsDir.static', 'j')
   expect(res).toEqual(['src', 'static'])
 })
 
-test('Not avail working directory array', () => {
+test('Working directory array for not key exists', () => {
   const res = TargetDirectory.wsArray('wsDir.json', 'data')
   expect(res).toEqual(['src', 'data'])
 })
 
-test('Null name working directory array', () => {
+test('Working directory array for key empty', () => {
   const res = TargetDirectory.wsArray('wsDir.unknown', 'html')
   expect(res).toEqual(['src', 'html'])
 })
 
-test('Avail working directory path', () => {
+test('Working directory path for key exists', () => {
   const res = TargetDirectory.wsPath('wsDir.js', 'js')
   expect(res).toBe('src/js')
 })
 
-test('Not avail working directory path', () => {
+test('Working directory path for not key exists', () => {
   const res = TargetDirectory.wsPath('wsDir.json', 'data')
   expect(res).toBe('src/data')
 })
