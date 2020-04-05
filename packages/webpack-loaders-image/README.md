ca35d0e8ed8af965f9a11eb0f99a48b58b74c915
Nobuyuki Kondo <kondo@arc-one.jp>
2020-04-05 22:13:49 +0900

    feat: Adding image loader

---
+++ packages/webpack-loaders-image/README.md
@@ -0,0 +1,65 @@
+# `@io-arc/webpack-loaders-image`
+
+Image deploy for using [file-loader](https://github.com/webpack-contrib/file-loader).  
+Auto insert image path to HTML, CSS.
+
+## Usage
+
+```typescript
+import { imageLoader } from '@io-arc/webpack-loaders-image'
+import { DEPLOY_IMG_ARRAY } from '@io-arc/env'
+
+export default {
+  // (abbreviation)
+  module: {
+    rules: [
+      imageLoader([], DEPLOY_IMG_ARRAY, true)
+    ]
+  }
+}
+```
+
+## Functions
+
+### `imageLoader(base: string[], target: [], isHash: boolean): RuleSetRule`
+
+Use [file-loader](https://github.com/webpack-contrib/file-loader) to get and build images with a specific extension.  
+Return types `RuleSetRule` for [webpack](https://webpack.js.org/)
+
+#### Parameters
+
+parameter | description | example
+--- | --- | ---
+base | file deployment destinations | `['common', 'js']`
+target | images deployment directory name array | `['common', 'js']` 
+isHash | image path adding hash in 6-digit | `true`
+
+#### example
+
+Structure:
+
+```text
+working:
+src/
+  ├─ html/
+  │   └─ index.html
+  └─ img/
+      └─ foo.png
+
+build:
+.
+├─ index.html
+└─ common/
+      └─ img/
+          └─ foo.png
+```
+
+```html
+<img src="img/foo.png" alt="">
+```
+
+deployment below:
+
+```html
+<img src="/common/img/foo.png" alt="">
+```
---
+++ packages/webpack-loaders-image/src/index.ts
@@ -0,0 +1,63 @@
+import { siteRootRelative, WS_IMG_PATH_ABSOLUTE } from '@io-arc/env'
+import OutputDirDiff from '@io-arc/output-dir-diff'
+import { TDirNameKey, TFileName } from '@io-arc/types'
+import { RuleSetRule } from 'webpack'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pngquant = require('imagemin-pngquant')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const gifscale = require('imagemin-gifsicle')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jpegtran = require('imagemin-jpegtran')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const svgo = require('imagemin-svgo')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const imageminLoader = require('imagemin-webpack').loader
+
+export const imageLoader = (
+  base: TDirNameKey[],
+  target: TDirNameKey[],
+  isHash: boolean
+): RuleSetRule => {
+  const dirDiff = new OutputDirDiff(base, target)
+  const inputPath = new RegExp(`${WS_IMG_PATH_ABSOLUTE}/`)
+
+  return {
+    test: /\.(png|jpe?g|gif|svg)$/i,
+    use: [
+      {
+        loader: 'file-loader',
+        options: {
+          name(file: TFileName): TFileName {
+            const arr = file.replace(inputPath, '').split('/')
+            arr.pop()
+
+            return `${arr.join('/')}/[name].[ext]${isHash ? '?[hash6]' : ''}`
+          },
+          outputPath: dirDiff.targetRelativePath(),
+          publicPath(url: string): TFileName {
+            const p = url.replace(/^\//, '')
+            return siteRootRelative(target) + p
+          },
+          esModule: false,
+          emitFile: true
+        }
+      },
+      {
+        loader: imageminLoader,
+        options: {
+          cache: true,
+          bail: false,
+          imageminOptions: {
+            plugins: [
+              gifscale({ interlaced: true }),
+              jpegtran({ progressive: true }),
+              pngquant({ quality: [0.8, 1] }),
+              svgo({ removeViewBox: true })
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
