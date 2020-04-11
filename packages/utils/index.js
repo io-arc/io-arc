fc33ec148bc94bc013411c0347dafd62197b72a3
Nobuyuki Kondo <kondo@arc-one.jp>
2020-04-11 13:16:21 +0900

    feat: adding utility library

---
+++ packages/utils/index.js
@@ -0,0 +1,9 @@
+/*!
+Utility functions
+
+https://github.com/io-arc/io-arc/packages/utils
+Version: 0.0.0
+License: ISC
+Copyright (c) 2020 arc one
+*/
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports,require("@io-arc/env")):"function"==typeof define&&define.amd?define(["exports","@io-arc/env"],t):t((e=e||self).Utils={},e.env)}(this,(function(e,t){"use strict";e.AssetsDirPath=e=>t.SITE_ROOT+e.join("/"),Object.defineProperty(e,"__esModule",{value:!0})}));
---
+++ packages/utils/types/index.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Website assets directory path
+ * @param dir - directory name array
+ * @constructor
+ */
+export declare const AssetsDirPath: (dir: string[]) => string;
